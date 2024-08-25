8767186239bca7751f8a0e5594790ad68d105261
lorilu-studio <1229166932@qq.com>
2024-08-25 22:50:24 +0800

    docusaurus-search-local 编译报错，隐藏

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,20 +14,21 @@ const config = {
     presets: require(path.join(__dirname, './config/presets')),
     themeConfig: require(path.join(__dirname, './config/theme')),
     plugins: require(path.join(__dirname, './config/plugins')),
-    themes: [
-        [
-            require.resolve('@easyops-cn/docusaurus-search-local'),
-            {
-                hashed: true,
-                language: ['en', 'zh'],
-                indexPages: true,
-                docsRouteBasePath: options.docs.routeBasePath,
-                blogRouteBasePath: options.blog.routeBasePath,
-                docsDir: options.docs.path,
-                blogDir: options.blog.path,
-            },
-        ],
-    ],
+    // 因为build报错，所以隐藏本地搜索插件：https://docs.wangchong.wang/blog/2023/06/08/docusaurus-search-local
+    // themes: [
+    //     [
+    //         require.resolve('@easyops-cn/docusaurus-search-local'),
+    //         {
+    //             hashed: true,
+    //             language: ['en', 'zh'],
+    //             indexPages: true,
+    //             docsRouteBasePath: options.docs.routeBasePath,
+    //             blogRouteBasePath: options.blog.routeBasePath,
+    //             docsDir: options.docs.path,
+    //             blogDir: options.blog.path,
+    //         },
+    //     ],
+    // ],
     // clientModules: [require.resolve('./src/styles/index.css')],
     i18n: {
         defaultLocale: 'zh-Hans',
